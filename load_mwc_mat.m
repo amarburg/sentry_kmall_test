ccdd1b9710b9af88fb700f920977ad1fb22cfe13
Aaron Marburg <amarburg@uw.edu>
2020-09-17 23:08:45 -0700

    Another incremental phase in matfile generation

--- load_mwc_mat.m
+++ load_mwc_mat.m
@@ -2,25 +2,11 @@ function out = load_mat( matfile )
 
 m = load(matfile);
 
-% Walk through MWC struture
-%out.mwc = cell2struct( m.data.MWC, {'temppings'}, 2 );
-
-out.mwc = [m.data.MWC{:}]
+% Python Matfiles come in with many additional layers of cell arrays
+% This compacts them out
+%
+out.mwc = [m.MWC{:}]
 
 for p = 1:numel(out.mwc)
-  %out.mwc(p).tempbeams = cell2struct( out.mwc(p).temppings.beams, {'tempbeams'}, 2);
-
-  %
-  % This is frustratingly manual
-  %
-  %for b = 1:numel(out.mwc(p).tempbeams)
-  %  out.mwc(p).beams(b).beamPointAngReVertical_deg = out.mwc(p).tempbeams(b).tempbeams.beamPointAngReVertical_deg;
-  %  out.mwc(p).beams(b).sampleAmplitude05dB_p      = out.mwc(p).tempbeams(b).tempbeams.sampleAmplitude05dB_p;
-  %  out.mwc(p).beams(b).rxBeamPhase_deg            = out.mwc(p).tempbeams(b).tempbeams.rxBeamPhase_deg;
-  %end
-
   out.mwc(p).beams = [out.mwc(p).beams{:}]
-
 end
-
-%out.mwc = rmfield( out.mwc, { 'temppings'} );
